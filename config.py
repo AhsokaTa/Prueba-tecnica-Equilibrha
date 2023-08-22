289a65a8d73590df607feac7c2f3b7c859307de6
AhsokaTa <sanchz.cris@gmail.com>
2023-08-22 23:52:57 +0200

    Implement config.py Add os module using environment variables

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+
 FILE_NAME = 'datos_prueba_tecnica.csv'
+
+
+"""
+  os.getenv(var_name , default) :obtener variable de entorno
+
+class Config:
+    FILE_NAME = os.getenv("FILE_NAME", "datos_prueba_tecnica.csv")
+"""
+
--- main.py
+++ main.py
@@ -10,3 +10,18 @@ if __name__ == "__main__":
     employee = EmployeeHandler(data_f)
 
     employee.print_results()
+
+"""
+==================================================================
+Implementar config.py -> importando os y crear clase Configr
+Para ajustar la configuración usando variables de entorno y mejorar
+    * Portabilidad
+    * Seguridad (sobre todo en entorno de producción)
+    * Escalabilidad (desplegar en multiples server)
+==================================================================
+modulo os (Operating System)
+    funciones 
+        - os.getenv(var_name , default) :obtener variable de entorno
+        - os.path.exists(path) : verifica si un archivo o directoria exist en ruta dada
+        - os.listdir(path): Otener lista de archivos y directorios en una ruta.
+"""
